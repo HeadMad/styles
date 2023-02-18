76586a087edf5fd8c042c5f52ade74b37eb62139
HeadMad <kov.ps@ya.ru>
2023-02-19 00:35:00 +0800

    add styles

---
+++ index.js
@@ -0,0 +1,13 @@
+import './css/root.css';
+import './css/body.css';
+import './css/a.css';
+import './css/hr.css';
+import './css/input.css';
+import './css/checkbox.css';
+import './css/radio.css';
+
+
+import './css/button.css';
+import './css/loadings.css';
+
+
